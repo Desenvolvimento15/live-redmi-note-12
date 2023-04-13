f48b7f35907cbc725543f83dd44065a84c15d8de
desenvolvimento15 <desenvolvimento15@dl.com.br>
2023-04-13 08:08:44 -0300

    vs01

--- assets/js/clebre-main.js
+++ assets/js/clebre-main.js
@@ -1,40 +1,36 @@
-window.onscroll = function() {scrollFunction()};
+$(document).ready(function() {
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-
-var modal = document.getElementById("myModal");
-
-
-var btn = document.getElementById("Btn");
-
-
-var span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+  // Gets the video src from the data-src on each button
+  
+  var $videoSrc;  
+  $('.video-btn').click(function() {
+      $videoSrc = $(this).data( "src" );
+  });
+  console.log($videoSrc);
+  
+    
+  // when the modal is opened autoplay it  
+  $('#myModal').on('shown.bs.modal', function (e) {
+      
+  // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+  $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+  
+  })
+    
+  
+  // stop playing the youtube video when I close the modal
+  $('#myModal').on('hide.bs.modal', function (e) {
+      // a poor man's stop video
+      $("#video").attr('src',$videoSrc); 
+  }) 
+      
+      
+  
+  
+    
+    
+  // document ready  
+  });
+  
+  
+  
